1ada4298bdac9ce10e6fd457d54b04c2c72a5820
Diego Fontalvo <d.andres2002@hotmail.com>
2026-06-07 21:40:34 -0500

    feat: add signout and token deletion logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from "@/hooks/use-theme";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { ReactNode } from "react";
 import { ThemedView } from "./ui/ThemedView";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -9,10 +9,12 @@ import { Ionicons } from "@expo/vector-icons";
 import { Spacing } from "@/constants/theme";
 import { getUser } from "@/api/auth";
 import { useAuth } from "@/hooks/use-auth";
+import { Loading } from "./ui/Loading";
 
 export const Header = (): ReactNode => {
   const theme = useTheme();
   const { signOut } = useAuth();
+  const { mutate, isPending } = useMutation({ mutationFn: signOut });
   const { data: user } = useQuery({ queryKey: ["user"], queryFn: getUser });
   return (
     <ThemedView>
@@ -21,8 +23,12 @@ export const Header = (): ReactNode => {
           <Ionicons name="person" size={24} color={theme.text} />
           <ThemedText>{user?.username}</ThemedText>
         </View>
-        <TouchableOpacity onPress={signOut}>
-          <Ionicons name="log-out-outline" size={24} color={theme.text} />
+        <TouchableOpacity onPress={() => mutate()} disabled={isPending}>
+          {isPending ? (
+            <Loading />
+          ) : (
+            <Ionicons name="log-out-outline" size={24} color={theme.text} />
+          )}
         </TouchableOpacity>
       </SafeAreaView>
     </ThemedView>
